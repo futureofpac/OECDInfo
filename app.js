b276838cdd589e743008970790a63348de9f75b2
Hoon Jung <nayahun2@gmail.com>
2013-05-06 21:33:32 +0200

    hello world12

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	console.log('Version: ' + process.version);
+	// console.log('Version: ' + process.version);
   res.send('hello world11');
 
 });
 
-// var port = process.env.PORT || 5000;
-// app.listen(port);
+var port = process.env.PORT || 5000;
+app.listen(port);
 
-app.listen(3000);
+// app.listen(3000);
