c845d4ad5fd08b7e28cabb4c78263b7a7dd33706
Hoon Jung <nayahun2@gmail.com>
2013-05-06 20:32:51 +0200

    hello world5

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 var express = require('express');
-
-var app = express.createServer(
-	express.logger(),
-	express.static(__dirname + '/public')
-);
+var app = express();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.send('hello world11');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function(){
-	console.log('Listening on ' + port);	
-});
+app.listen(3000);
