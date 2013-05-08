6d24eefd2033ed05afc537f195bbee1c4879fb20
Hoon Jung <nayahun2@gmail.com>
2013-05-08 20:52:05 +0200

    twitter 1

---
+++ twitter.js
@@ -0,0 +1,65 @@
+var Twit = require('twit')
+
+// Consumer key	YESBKjjb6RtIsDfKJbf1Q
+// Consumer secret	ottkkdctP55j0VcDrG41nuLXD51FB9ab7KcnWLs
+// Request token URL	https://api.twitter.com/oauth/request_token
+// Authorize URL	https://api.twitter.com/oauth/authorize
+// Access token URL	https://api.twitter.com/oauth/access_token
+// Callback URL	http://pariscafe.herokuapp.com
+
+var T = new Twit({
+    consumer_key:         'YESBKjjb6RtIsDfKJbf1Q'
+  , consumer_secret:      'ottkkdctP55j0VcDrG41nuLXD51FB9ab7KcnWLs'
+  , access_token:         '92711180-XKHDV6E0yFfjk92NHmR6c9nllUAq4pwcLTU27WA'
+  , access_token_secret:  'yQLc0KEEurMlT78xO4MjztUIekpjCGCwaP8XGXoSsbk'
+})
+
+//
+//  tweet 'hello world!'
+//
+T.post('statuses/update', { status: 'hello world!' }, function(err, reply) {
+  //  ...
+})
+
+//
+//  search twitter for all tweets containing the word 'banana' since Nov. 11, 2011
+//
+T.get('search/tweets', { q: 'banana since:2011-11-11' }, function(err, reply) {
+  //  ...
+})
+
+//
+//  get the list of user id's that follow @tolga_tezel
+//
+T.get('followers/ids', { screen_name: 'tolga_tezel' },  function (err, reply) {
+  //  ...    
+})
+
+//
+//  stream a sample of public statuses
+//
+var stream = T.stream('statuses/sample')
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
+
+//
+//  filter the twitter public stream by the word 'mango'. 
+//
+var stream = T.stream('statuses/filter', { track: 'mango' })
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
+
+//
+// filter the public stream by the latitude/longitude bounded box of San Francisco
+//
+var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
+
+var stream = T.stream('statuses/filter', { locations: sanFrancisco })
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+})
