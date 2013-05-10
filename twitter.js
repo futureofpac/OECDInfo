b497fd88441494fa5df0bf7b1508beb44d9cde46
Hoon Jung <nayahun2@gmail.com>
2013-05-10 20:07:04 +0200

    i got it

--- twitter.js
+++ twitter.js
@@ -7,6 +7,8 @@ var Twit = require('twit')
 // Access token URL	https://api.twitter.com/oauth/access_token
 // Callback URL	http://pariscafe.herokuapp.com
 
+console.log('run twit');
+
 var T = new Twit({
     consumer_key:         'YESBKjjb6RtIsDfKJbf1Q'
   , consumer_secret:      'ottkkdctP55j0VcDrG41nuLXD51FB9ab7KcnWLs'
@@ -25,20 +27,24 @@ T.post('statuses/update', { status: 'hello world!' }, function(err, reply) {
 //  search twitter for all tweets containing the word 'banana' since Nov. 11, 2011
 //
 T.get('search/tweets', { q: 'banana since:2011-11-11' }, function(err, reply) {
+	// console.log('search/tweets:');
+	// console.log(reply);
   //  ...
 })
 
 //
 //  get the list of user id's that follow @tolga_tezel
 //
-T.get('followers/ids', { screen_name: 'tolga_tezel' },  function (err, reply) {
+T.get('statuses/user_timeline', { screen_name: 'oecd' },  function (err, reply) {
+	console.log('oecd start');
+	console.log(reply);
   //  ...    
 })
 
 //
 //  stream a sample of public statuses
-//
-// var stream = T.stream('statuses/sample')
+
+var stream = T.stream('statuses/sample')
 
 // stream.on('tweet', function (tweet) {
 //   console.log(tweet)
