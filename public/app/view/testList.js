93290690ea842b5c2b9ce01408116bd51a653cf8
Hoon Jung <nayahun2@gmail.com>
2013-05-30 16:45:31 +0200

    new list

--- public/app/view/testList.js
+++ public/app/view/testList.js
@@ -23,6 +23,55 @@ Ext.define('OECDInfo.view.testList', {
             }
         ],
         emptyText:'<div style="padding:10px;">No Data</div>',
-        itemTpl:'<div style="font-size:small">{typeName} - {title} ({pubDate})</div>'
+        // itemTpl:'<div style="font-size:small">{typeName} - {title} ({pubDate})</div>'
+        itemTpl: 
+            new Ext.XTemplate(
+                '<tpl if="this.isFlickr(typeName)">',
+                    '<div style="background-image:url({image}_n.jpg);background-repeat:no-repeat;background-position:center center;width:100%;height:180px;"></div>',
+                    '<div style="overflow:hidden;text-overflow:ellipsis;padding-top:7npx;">{title:ellipsis(90, true)}</div>',
+                '<tpl else>',
+                    '<tpl if="this.hasImage(image)">',
+                    '<div style="min-height:60px;font-family:Arial, sans-serif;">',
+                        '<tpl if="this.isTwitter(type)">',
+                            '<img src="{image}" style="float:left;width:45px;-webkit-border-radius:5px;">',
+                            '<div style="margin-left:55px;">',
+                        '<tpl else>',
+                            '<img src="{image}" style="float:left;width:80px;-webkit-border-radius:5px;">',
+                            '<div style="margin-left:90px;">',
+                        '</tpl>',
+                            '<div style="color:#989898;margin-bottom:8px;">{typeName}</div>',
+                            '<div>{title}</div>',
+                            '<div style="clear:both;margin-bottom:2px;"></div>',
+                        '</div>',
+                    '</div>',
+                    '<tpl else>',
+                        '<div style="color:#989898;margin-bottom:8px;">{typeName}</div>',
+                        '<div style="margin-left:5px;margin-bottom:2px;">{title}</div>',
+                    '</tpl>',
+                '</tpl>',
+                {
+                hasImage: function(image){
+                    if(image != null && image != ""){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                },
+                isTwitter: function(type){
+                    if(type == 'Twitter'){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                },
+                isFlickr: function(type){
+                    if(type == 'Flickr'){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        )           
     }
 });
